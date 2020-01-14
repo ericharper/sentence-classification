2b70fd3cb759790378a10ab3b6f108e2f0afaaaa
ericharper <complex451@gmail.com>
2020-01-14 14:46:14 -0700

    added script to download SST-2

---
+++ get_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -d ./data ]; then
+    mkdir ./data
+elif [ -d ./data ]; then
+    echo "data directory already exists"
+fi
+
+if [ -e ./data/SST-2.zip ]; then
+    echo "SST-2.zip already exists"
+elif [ ! -e ./data/SST-2.zip ]; then
+    curl 'https://firebasestorage.googleapis.com/v0/b/mtl-sentence-representations.appspot.com/o/data%2FSST-2.zip?alt=media&token=aabc5f6b-e466-44a2-b9b4-cf6337f84ac8' -o ./data/SST-2.zip
+fi
