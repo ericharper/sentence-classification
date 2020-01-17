ac23eef65464241aa1543fafbf423c915782ac5b
ericharper <complex451@gmail.com>
2020-01-17 10:09:53 -0700

    created seperate notebook for inference

--- get_data.sh
+++ get_data.sh
@@ -10,4 +10,6 @@ if [ -e ./data/SST-2.zip ]; then
     echo "SST-2.zip already exists"
 elif [ ! -e ./data/SST-2.zip ]; then
     curl 'https://firebasestorage.googleapis.com/v0/b/mtl-sentence-representations.appspot.com/o/data%2FSST-2.zip?alt=media&token=aabc5f6b-e466-44a2-b9b4-cf6337f84ac8' -o ./data/SST-2.zip
+    cd data
+    unzip SST-2.zip
 fi
